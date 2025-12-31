35609b21daa939346bcb226bcffbe03930e50e31
ximhi20 <ximhi20@tutamail.com>
2025-12-31 12:54:42 +0100

    Añadidas mejoras en las tablas.

--- js_pruebas/js/script4.js
+++ js_pruebas/js/script4.js
@@ -1,10 +1,11 @@
 let tablas =prompt("Dime hasta que tabla quieres llegar.");
-let numero = [];
-for(let i=0; i<=tablas; i++) {
-        numero.push(i);
+// hacer un Array "como bucle".
+let numero = []; /* Se crea la array que quieres hacer. */
+for(let i=0; i<=tablas; i++) { /* Se hace un bucle en el que se mete el "resultado" en la array. */
+        numero.push(i); /* El "push" se usa poniendo entre paréntesis lo que quieres meter en la array. */
 }
 
-let miHTML='<h1>Tablas de multiplicar</h1><section>';
+let miHTML='<h1>Tablas de multiplicar hasta la del '+tablas+'</h1><section>';
 for (let i = 1; i < numero.length; i++) {
     miHTML+='<div class="destaque"><h2>Tabla del '+numero[i]+'</h2><ul>';
     for (let i2 = 1; i2 <= 10; i2++) {
