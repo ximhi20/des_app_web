f83ff37fba984105d957b76173e96c2790cf2e4a
ximhi20 <ximhi20@tutamail.com>
2025-12-31 12:45:19 +0100

    Completadas las tablas.

--- js_pruebas/js/script3.js
+++ js_pruebas/js/script3.js
@@ -1,6 +1,6 @@
 let nacimiento=prompt('¿En que año naciste?');
 let currentYear=new Date().getFullYear();
 
-let miHTML='<p>Si naciste en '+nacimiento+' y como estamos en el año '+currentYear+', entonces tienes (o tendrás este año): '+(currentYear-nacimiento)+' años. </p>';
+let miHTML='<div><p>Si naciste en '+nacimiento+' y como estamos en el año '+currentYear+', entonces tienes (o tendrás este año): '+(currentYear-nacimiento)+' años. </p></div>';
 
 document.body.innerHTML=miHTML;
--- js_pruebas/js/script4.js
+++ js_pruebas/js/script4.js
@@ -1,15 +1,17 @@
-let numero = []
-for (let i3 = 1; i3 <= 9; i3++) {
-    numero += i3;
+let tablas =prompt("Dime hasta que tabla quieres llegar.");
+let numero = [];
+for(let i=0; i<=tablas; i++) {
+        numero.push(i);
 }
 
-let miHTML='<h1>Tablas de multiplicar</h1>';
-for (let i = 0; i < numero.length; i++) {
-    miHTML+='<h2>Tabla del '+numero[i]+'</h2><ol>';
+let miHTML='<h1>Tablas de multiplicar</h1><section>';
+for (let i = 1; i < numero.length; i++) {
+    miHTML+='<div class="destaque"><h2>Tabla del '+numero[i]+'</h2><ul>';
     for (let i2 = 1; i2 <= 10; i2++) {
-        miHTML += '<li>'+numero[i]+'*'+i2+'='+i2*numero[i]+'</li>';
+        miHTML += '<li>'+numero[i]+'*'+i2+'='+(i2*numero[i])+'</li>';
     };
-    miHTML+='</ol>';
+    miHTML+='</ul></div>';
 };
+miHTML+='</section>'
 
 document.body.innerHTML=miHTML;
