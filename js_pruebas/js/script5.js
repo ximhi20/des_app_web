0a92dfa2d6d6cd3d357ad2fed105530536d07428
ximhi20 <ximhi20@tutamail.com>
2026-01-08 10:43:54 +0100

    Calendario mejorado.

--- js_pruebas/js/script5.js
+++ js_pruebas/js/script5.js
@@ -13,11 +13,39 @@ let mes=[
     'Diciembre'
 ];
 
+let daysNumber=[
+    31,
+    28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+];
+
+let dayName=[
+    'L',
+    'M',
+    'X',
+    'J',
+    'V',
+    'S',
+    'D'
+];
+
 miHTML='';
 for (let i = 0; i < mes.length; i++) {
     miHTML+='<div class="month"><h2>'+mes[i]+'</h2><ul>';
-    for (let i2 = 1; i2 <= 30; i2++) {
-        miHTML+='<li>'+i2+'</li>';
+    for (let i3 = 0; i3 < dayName.length; i3++) {
+        miHTML+='<li class="dia">'+dayName[i3]+'</li>';
+    };
+    for (let i2 = 1; i2 <= daysNumber[i]; i2++) {
+        miHTML+='<li class="'+mes[i]+'">'+i2+'</li>';
     };
     miHTML+='</ul></div>';
 };
