0db82dfc6c235fb5f552cbdd8503f5cd4851debc
ximhi20 <ximhi20@tutamail.com>
2026-02-05 10:37:38 +0100

    Terminado el KH.

--- kingdom_hearts/js/script.js
+++ kingdom_hearts/js/script.js
@@ -1,11 +1,12 @@
+// Lista con la información sobre los distintos personajes y Mundos.
 const LIST = {
-    char: {
-        main: [
+    char: { // Los personajes.
+        main: [ // Los principales.
             {
-                name: 'Sora',
-                url: 'img/personajes/protas/sora.webp',
-                alt: 'Sora de Kingdom Hearts III.',
-                desc: 'El protagonista y a quien controlas en la mayoría de los juegos.'
+                name: 'Sora', // Nombre del personaje o mundo.
+                url: 'img/personajes/protas/sora.webp', // URL para la imagen.
+                alt: 'Sora de Kingdom Hearts III.', // Alt de la imagen.
+                desc: 'El protagonista y a quien controlas en la mayoría de los juegos.' // la información.
             },
             {
                 name: 'Aqua, Terra y Ventus',
@@ -20,7 +21,7 @@ const LIST = {
                 desc: 'Personaje principal de KH 358/2 Days.'
             }
         ],
-        ant: [
+        ant: [ // Los antagonistas.
             {
                 name: 'Xehanort',
                 url: 'img/personajes/antagonistas/xehanort.webp',
@@ -34,22 +35,70 @@ const LIST = {
                 desc: 'Un grupo antagonístico recurrente en varios juegos.'
             }
         ]
+    },
+    world: { // Los Mundos.
+        orig: [ // Los originales de KH.
+            {
+                name: 'Islas del Destino',
+                url: 'img/mundos/originales/destiny_islands.png',
+                alt: 'Representación de las Islas del Destino.',
+                desc: 'Mundo original de Sora y Riku, de donde desean salir para visitar otros mundos.'
+            },
+            {
+                name: 'Villa Crepúsculo',
+                url: 'img/mundos/originales/traverse_town.png',
+                alt: 'Representación de Villa Crepúsculo.',
+                desc: 'Mundo en el que Roxas se encuentra durante el tutorial de KH II.'
+            },
+            {
+                name: 'Ciudad de Paso',
+                url: 'img/mundos/originales/twilight_town.png',
+                alt: 'Representación de Ciudad de Paso.',
+                desc: 'Mundo en el que se encuentran aquellos personajes que hallan perdido el suyo por culpa de los Sincorazón.'
+            }
+        ],
+        disney: [ // Los que salieron de Disney.
+            {
+                name: 'Castillo Disney',
+                url: 'img/mundos/disney/disney_castle.png',
+                alt: 'Representación del Castillo Disney.',
+                desc: 'Mundo en el que se encuentran varios personajes Disney que no tienen uno "oficial".'
+            },
+            {
+                name: 'Wonderland',
+                url: 'img/mundos/disney/wonderland.png',
+                alt: 'Representación de Wonderland.',
+                desc: 'Mundo representando la película animada de Alicia en el País de las Maravillas.'
+            }
+        ]
     }
 };
 
+// Función para crear las listas de personajes y mundos, con "rele" como la parte de LIST a usar y "id" la id del elemento a "sustituir".
 function lista(rele, id) {
     let laLista = '';
-    for (let i=0; i<rele.length; i++) {
+    let rel = rele;
+    for (let i=0; i<rel.length; i++) {
         laLista += `
             <div>
-                <h3>${rele[i].name}</h3>
-                <img src="${rele[i].url}" alt="${rele[i].alt}">
-                <p>${rele[i].desc}</p>
+                <h3>${rel[i].name}</h3>
+                <img src="${rel[i].url}" alt="${rel[i].alt}">
+                <p>${rel[i].desc}</p>
             </div>
         `;
     };
     document.querySelector(`#${id}`).innerHTML = laLista;
-}
+};
 
-lista(LIST.char.main, 'char_prin');
-lista(LIST.char.ant, 'char_ant')
+
+// Se usa la función varias veces para sacar las distintas listas que se requieren (hay que empezar por LIST y pasar por todos los "puntos necesarios" excepto por el "número" y el último punto, con un "if" para que solo saque los ids que existan.
+function lanzar(tipo) {
+    if (tipo=='pers'){
+        lista(LIST.char.main, 'char_prin'); // Lista de personajes principales.
+        lista(LIST.char.ant, 'char_ant'); // Lista de antagonistas.
+    }
+    else if(tipo=='mund'){
+        lista(LIST.world.orig, 'mun_ori'); // Lista de Mundos originales.
+        lista(LIST.world.disney, 'mun_dis'); // Lista de Mundos Disney.
+    }
+};
