39cfca842a53f2d8b427b4dce0311db089fad47e
ximhi20 <ximhi20@tutamail.com>
2026-02-06 13:57:08 +0100

    Update index.php

--- get_php/index.php
+++ get_php/index.php
@@ -11,10 +11,15 @@
 </head>
 <body>
     <header>
-        <h1>Mi juego favorito es <?php echo $juego; ?>.</h1>
+        <ul>
+            <li><a href="?game=Kingdom%20Hearts%20II">Kingdom Hearts II</a></li>
+            <li><a href="?game=Horizon%20Zero%20Dawn">Horizon Zero Dawn</a></li>
+            <li><a href="?game=Dragon%20Age:%20Origins">Dragon Age: Origins</a></li>
+        </ul>
     </header>
 
     <main>
+        <h1>Mi juego favorito es <?php echo $juego; ?>.</h1>
         <p>Este juego (<?php echo $juego; ?>) es muy interesante.</p>
         <p>Aunque quizá te gusten otros juegos, <?php echo $juego; ?> es el que me agrada a mi.</p>
     </main>
