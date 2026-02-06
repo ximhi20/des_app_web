4365eaf199ed74aee41660b564f456c200f6be84
ximhi20 <ximhi20@tutamail.com>
2026-02-06 13:50:17 +0100

    Update index.php

--- get_php/index.php
+++ get_php/index.php
@@ -7,20 +7,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $juego ?></title>
+    <title><?php echo $juego; ?></title>
 </head>
 <body>
     <header>
-        <h1>Mi juego favorito es <?php echo $juego ?>.</h1>
+        <h1>Mi juego favorito es <?php echo $juego; ?>.</h1>
     </header>
 
     <main>
-        <p>Este juego (<?php echo $juego ?>) es muy interesante.</p>
-        <p>Aunque quizá te gusten otros juegos, <?php echo $juego ?> es el que me agrada a mi.</p>
+        <p>Este juego (<?php echo $juego; ?>) es muy interesante.</p>
+        <p>Aunque quizá te gusten otros juegos, <?php echo $juego; ?> es el que me agrada a mi.</p>
     </main>
 
     <footer>
-        <p>&copy; <?php echo $juego ?></p>
+        <p>&copy; <?php echo $juego; ?></p>
     </footer>
 </body>
 </html>
