09b0d75d950bf520585a0c251077ece96dc096aa
ximhi20 <ximhi20@tutamail.com>
2026-02-06 12:20:49 +0100

    Nuevo ejercicio.

---
+++ repeticion/index.php
@@ -0,0 +1,26 @@
+<?php
+    $juego = "Kingdom Hearts II";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $juego ?></title>
+</head>
+<body>
+    <header>
+        <h1>Mi juego favorito es <?php echo $juego ?>.</h1>
+    </header>
+
+    <main>
+        <p>Este juego (<?php echo $juego ?>) es muy interesante.</p>
+        <p>Aunque quizá te gusten otros juegos, <?php echo $juego ?> es el que me agrada a mi.</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo $juego ?></p>
+    </footer>
+</body>
+</html>
