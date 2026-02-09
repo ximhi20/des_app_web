40c8e95b12a664ac492d26a7cc0727e351d78975
ximhi20 <ximhi20@tutamail.com>
2026-02-09 14:00:47 +0100

    Creación carpeta.

---
+++ galeria_php/assets/include/juegos-kh.php
@@ -0,0 +1,76 @@
+<?php
+    $fichas = [
+        [
+            "nombre" => "Kingdom Hearts Final Mix",
+            "enlace" => "../img/portadas/fm.png",
+            "altern" => "Logo de la versión Final Mix del primer juego.",
+            "descri" => "El primer juego de la saga, donde empiezan las aventuras de Sora y sus amigos,"
+        ],
+        [
+            "nombre" => "Kingdom Hearts Re:Chain of Memories",
+            "enlace" => "../img/portadas/recom.png",
+            "altern" => "Logo de Re:Chain of Memories.",
+            "descri" => "Sora, Donald y Goofy llegan a un castillo en el que pierden sus recuerdos según van avanzando"
+        ],
+        [
+            "nombre" => "Kingdom Hearts 358/2 Days",
+            "enlace" => "../img/portadas/days.png",
+            "altern" => "Logo de 358/2 Days",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts II Final Mix",
+            "enlace" => "../img/portadas/iifm.png",
+            "altern" => "Logo de la versión Final Mix del segundo juego.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts Birth by Sleep Final Mix",
+            "enlace" => "../img/portadas/bbs.png",
+            "altern" => "Logo de la versión Final Mix de Birth by Sleep.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts Re:coded",
+            "enlace" => "../img/portadas/recoded.png",
+            "altern" => "Logo de Re:coded.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts Dream Drop Distance HD",
+            "enlace" => "../img/portadas/ddd.png",
+            "altern" => "Logo de la versión HD de Dream Drop Distance.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts III Re Mind",
+            "enlace" => "../img/portadas/iiirm.png",
+            "altern" => "Logo del tercer juego junto con su DLC Re Mind.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts Chi",
+            "enlace" => "../img/portadas/chi.png",
+            "altern" => "Logo de Chi.",
+            "descri" => "El juego de navegador de KH, tratando sobre los eventos que llevan a la Guerra de las Llaves Espada."
+        ],
+        [
+            "nombre" => "Kingdom Hearts Union Cross",
+            "enlace" => "../img/portadas/unioncross.png",
+            "altern" => "Logo de Union Cross.",
+            "descri" => "El juego de móbil de KH, aunque, a diferencia de lo que se puede pensar, es uno completamente didtinto al de navegador web (es una secuela)."
+        ],
+        [
+            "nombre" => "Kingdom Hearts Dark Road",
+            "enlace" => "../img/portadas/dr.png",
+            "altern" => "Logo de Dark Road.",
+            "descri" => ""
+        ],
+        [
+            "nombre" => "Kingdom Hearts Melody of Memory",
+            "enlace" => "../img/portadas/mom.png",
+            "altern" => "Logo de Melody of Memory",
+            "descri" => ""
+        ]
+    ]
+?>
