3a75011e09c4e629318806b7f2bc8cbaf84a46f2
ximhi20 <ximhi20@tutamail.com>
2026-02-12 11:10:28 +0100

    Creada una referencia para la posterioridad.

---
+++ __ignorar/index.php
@@ -0,0 +1,55 @@
+<!-- Para dejar constancia de como hice para que pueda entrar en los directorios que necesite automáticamente, suponiendo que se está en una carpeta creada por "Local" en la que entraste en la carpeta "public", dentro de "app", borraste todo y se puso la carpeta con los directorios de GitHub. -->
+
+<?php
+    $path = "GitHub/des_app_web/*";
+    $dir = glob($path, GLOB_ONLYDIR);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Direcciones</title>
+    <style>
+        html{
+            text-align: center;
+            background-color: lightblue;
+        }
+
+        ul{
+            list-style: none;
+        }
+
+        li{
+            margin: 5px;
+        }
+
+        a{
+            text-decoration: none;
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>¿A dónde quieres ir?</h1>
+    </header>
+
+    <main>
+        <ul>
+            <?php
+                foreach ($dir as $folder) {
+                    $exp = explode("/", $folder);
+                    $name = $exp[2];
+                    echo "<li><a href='$folder'>$name</a></li>";
+                };
+            ?>
+        </ul>
+    </main>
+
+    <footer>
+        <p>&copy; Selección.</p>
+    </footer>
+</body>
+</html>
